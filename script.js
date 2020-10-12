4a7d76de66315c2424199184b35122d1bdddab13
mdalvin <mochamaddalvin@gmail.com>
2020-10-12 23:05:34 +0700

    using function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // Assignment III
 
 // No. 1 : Make a program that can give information about days.
+
 console.log("=== No. 1 : About days. ===");
 
 let days = [
@@ -29,41 +30,51 @@ for (let i = 0; i <= days.length; i++) {
     }
 
 }
+
 // No. 1 : end
 
 // No. 2 : Grading based on score.
 
 console.log("=== No.2 : Grading based on score. === ");
 
-let score = 32
-
-if (score >= 90 && score <= 100){
-    console.log(`Your score is ${score}. You got an 'A'.`)
-} else if (score >= 75 && score <= 89) {
-    console.log(`Your score is ${score}. You got a 'B'.`)
-} else if (score >= 60 && score <= 74) {
-    console.log(`Your score is ${score}. You got a 'C'.`)
-} else if (score >= 50 && score <= 59) {
-    console.log(`Your score is ${score}. You got a 'D'.`)
-} else {
-    console.log(`Your score is ${score}. You have failed. Try harder next time!.`)
+function scoreCheck(score){
+    if (score >= 90 && score <= 100){
+        console.log(`Your score is ${score}. You got an 'A'.`)
+    } else if (score >= 75 && score <= 89) {
+        console.log(`Your score is ${score}. You got a 'B'.`)
+    } else if (score >= 60 && score <= 74) {
+        console.log(`Your score is ${score}. You got a 'C'.`)
+    } else if (score >= 50 && score <= 59) {
+        console.log(`Your score is ${score}. You got a 'D'.`)
+    } else {
+        console.log(`Your score is ${score}. You have failed. Try harder next time!.`)
+    }
 }
 
 // No. 2 : end
 
-// No. 3 and 4 : Looping either odd or even numbers.
+// No. 3 and 4 : Looping of odd and even number (negative number as well).
 
-console.log("=== No.3 and 4 : Looping either odd or even numbers. ===")
+console.log("=== No.3 and 4 : Looping of odd and even number. ===")    
 
-let number = 29
-
-if (number % 2 === 0){
-    for (let i = 2; i <= number; i+=2){
-        console.log(i);
-    }
-} else{
-    for (let i = 1; i <= number; i+=2){
-        console.log(i);
+function numberOddEven(number){
+    if (number % 2 === 0){
+        if (number < 0){
+            for (let i = number; i <= 0; i+=2){
+                console.log(i);
+            } 
+        } else {
+            for (let i = 2; i <= number; i+=2){
+                console.log(i);}
+        }
+    } else if (number < 0){
+        for (let i = number; i <= 0; i+=2){
+            console.log(i);
+        }
+    } else {
+        for (let i = 1; i <= number; i+=2){
+            console.log(i);
+        }
     }
 }
 
